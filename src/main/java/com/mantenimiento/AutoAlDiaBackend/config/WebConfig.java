93ca139591ccb72e85b63f8f061348d780e71ad5
brunoSacchetti <sacchettibruno03@gmail.com>
2026-03-05 20:02:25 -0300

    feat: Agregado de Cors para permitir origen del Frontend

---
+++ src/main/java/com/mantenimiento/AutoAlDiaBackend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.mantenimiento.AutoAlDiaBackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todos los endpoints
+                .allowedOrigins("http://localhost:5173") // Permitir el puerto del Frontend (Vite)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
